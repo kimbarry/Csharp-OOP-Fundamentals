bed69dc99ddcbec9bf22686b6972d94b79591a8d
Kim Barry <kimbarrymail@gmail.com>
2023-04-12 18:30:53 -0400

    add gitignore, and new classes

---
+++ BethanysPieShop.InventoryManagement/BethanysPieShop.InventoryManagement/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.InventoryManagement
+{
+    public class Product
+    {
+        private int id;
+        private string name = string.Empty;
+        private string? description;
+
+        private int maxItemsInStock = 0;
+
+        private UnitType unitType;
+        private int amountInStock = 0;
+        private bool isBelowStockThreshold = false;
+    }
+}
